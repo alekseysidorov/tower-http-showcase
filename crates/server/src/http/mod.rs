dc01815b7d769d60f19a04c23b610704b28f8887
Aleksey Sidorov <sauron1987@gmail.com>
2025-01-11 14:36:46 +0300

    Polish code

--- crates/server/src/http/mod.rs
+++ crates/server/src/http/mod.rs
@@ -2,10 +2,24 @@ use axum::{routing::get, Router};
 
 use crate::state::SharedAppState;
 
-mod routes;
-
 pub fn make_router(state: SharedAppState) -> Router {
     Router::new()
         .route("/hello", get(routes::hello_world))
         .with_state(state)
+}
+
+mod routes {
+    use axum::{extract::State, Json};
+    use showcase_api::model::{HelloRequest, HelloResponse};
+
+    use crate::state::SharedAppState;
+
+    pub async fn hello_world(
+        State(_state): State<SharedAppState>,
+        Json(request): Json<HelloRequest>,
+    ) -> Json<HelloResponse> {
+        Json(HelloResponse {
+            message: format!("Hello, {}!", request.name),
+        })
+    }
 }
